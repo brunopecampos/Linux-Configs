e4c334e7533e1760b26610060e87b548cefd55cd
brunopecampos <brunopecampos@protonmail.com>
2025-03-03 23:18:33 +0100

    Update hosts file + change keyboard layout to us altgr-intl
    Signed-off-by: brunopecampos <brunopecampos@protonmail.com>

--- keys/install.sh
+++ keys/install.sh
@@ -2,14 +2,9 @@
 # Install xmodmap and xbindkey
 sudo pacman -s xorg-xmodmap xbindkey
 # Add xmodmap and xbindkey config files to home
-cp ./.Xmodmap ./.xbindkeysrc ./.switchlanguage.sh ~/
-# Give execution permition to switch language script
-chmod +x ~/.switchlanguage.sh
-# Set language to portuguese and then switch to english, loading the xmodmap shortcups through the switchlanguage script
-setxkbmap br
-~/.switchlanguage.sh
+cp ./.Xmodmap ./.xbindkeysrc ~/
+# Set layout to us, variant altgr-intl
+# This command will persist the configuration in /etc/X11/xorg.conf.d/00-keyboard.conf
+localectl --no-convert set-x11-keymap us us altgr-intl
 # Add xbindkeys keys command to /etc/lightdm/Xsession
-#
-#
-#
-# You can switch between br and us layouts with caps + ijkl key remapping
+# You use arrows with caps + ijkl key remapping
